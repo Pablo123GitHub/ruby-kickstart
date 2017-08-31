3e6ade2be9901bc7dfb266a5de7613e0880bf2f4
Pablo <pabloandresvdl@gmail.com>
2017-08-31 22:12:55 +0100

    answer to 1_input_output.rb

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -3,10 +3,20 @@
 
 # Write a program that reads in two integers typed on the keybaord
 # and outputs their sum, difference, and product
-# 
+#
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
   # your code goes here
+
+  user_response = gets
+
+  puts "#{user_response}"
+
+a = user_response.split(" ")[0].to_i
+b = user_response.split(" ")[1].to_i
+
+puts "#{a+b}" + "\n" + "#{a-b}" + "\n" + "#{a*b}" + "\n"
+
 end
