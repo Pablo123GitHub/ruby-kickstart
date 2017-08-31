06cb48da2ac220183d4acbd61068a4ae69a771d8
Pablo <pabloandresvdl@gmail.com>
2017-08-31 20:56:16 +0100

    answer to 7_string.rb

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,19 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  resultArr = []
+
+  string.split("").each_index do |x|
+   if (string[x].downcase === "r" && string[x+1] != nil)
+     resultArr.push(string[x+1])
+   end
+  end
+
+  return resultArr.join("")
+
+
 end
