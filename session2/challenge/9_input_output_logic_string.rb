de675257aa18034be9d7dfcaea2278886b844f93
Pablo <pabloandresvdl@gmail.com>
2017-09-02 16:38:06 +0100

    answer to 9_input_output_logic_string.rb

--- session2/challenge/9_input_output_logic_string.rb
+++ session2/challenge/9_input_output_logic_string.rb
@@ -25,7 +25,18 @@
 
 def deaf_grandma
 
+  while user_response = gets.chomp!
+   break if user_response === "BYE"
+  if user_response.upcase === user_response && user_response != ""
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
 end
+
+end
+
+
 
 
 
