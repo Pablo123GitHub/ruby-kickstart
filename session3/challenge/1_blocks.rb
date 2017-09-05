fea0c437ea53034373583a2c87dd2b92747da103
Pablo <pabloandresvdl@gmail.com>
2017-09-05 21:39:27 +0100

    answer to session3/challenge/1_blocks.rb

--- session3/challenge/1_blocks.rb
+++ session3/challenge/1_blocks.rb
@@ -3,3 +3,14 @@
 #
 # reverse_map(1, 2, 3) { |i| i * 2 }      # => [6, 4, 2]
 
+def reverse_map(*num)
+  result = []
+num.to_a.each do |element|
+  
+  result <<( yield element )
+end 
+  result.reverse
+   
+  
+end 
+
