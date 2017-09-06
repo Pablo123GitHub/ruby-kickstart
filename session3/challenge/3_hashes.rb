6aafe0c1f66cc2e4f293cb7196714690b040f342
Pablo <pabloandresvdl@gmail.com>
2017-09-06 23:07:37 +0100

    answer to session3/challenge/3_hashes.rb

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,21 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count (inputString)
+  inputArr = inputString.downcase.split(" ")
+
+newHash = inputArr.uniq.group_by{|word|
+inputArr.count(word) }
+
+ brandNewHash = Hash.new
+
+newHash.each do | value, words |
+ words.each do |x|
+   brandNewHash[x] = value
+ end
+end
+
+brandNewHash
+
+end
